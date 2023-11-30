15429aee33fdf55f99cd087cce3a2d918a549958
wein <basaria@naver.com>
2023-11-30 16:48:24 +0900

    provider를 이용한 변수 정보 전송작업 - AuthProvider.jsx 생성, logout 코딩

--- src/pages/ChatLobby.jsx
+++ src/pages/ChatLobby.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useAuthValue } from '../context/AuthProvider'
+
+import '../assets/css/chatlobby.css';
+import Header from '../layout.jsx/Header';
+import Footer from '../layout.jsx/Footer';
 
 const ChatLobby = () => {
+  const { email, nick, uicon} = useAuthValue();
   return (
-    <div>ChatLobby</div>
+    <>
+      <Header nick={nick} uicon={uicon} />
+      <Footer />
+    </>
   )
 }
 
